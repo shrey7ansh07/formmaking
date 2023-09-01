76deb25ef2a92f1e31aec9594380e1eac174367b
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-09-02 00:37:25 +0530

    made several modification like highlighting important form feilds when they are correct or wrong

--- script.js
+++ script.js
@@ -10,40 +10,69 @@ const submit = document.querySelector(".myform");
 
 submit.addEventListener("submit", validate);
 phone.addEventListener("click",()=>{
-  document.querySelector("#phonelength").style.display = "none";
   phone.classList.remove("addborder")
 })
 password.addEventListener("click",()=>{
-  document.querySelector("#passwordsuggest").style.display = "none";
   password.classList.remove("addborder")
 });
 c_password.addEventListener("click",()=>
 {
-  document.querySelector("#c_passwordsuggest").style.display = "none";
   c_password.classList.remove("addborder")
 
 })
 
+
 function validate(e) {
-    e.preventDefault();
+  
   if(phone.value.length!=10)
   {
-    document.querySelector("#phonelength").style.display = "block"
+    phone.setCustomValidity("mobile no should be of 10 digits")
+    phone.reportValidity();  
     phone.classList.add("addborder")
+  }
+  else
+  {
+    phone.classList.remove("addborder")
+    phone.classList.add("addborderpos")
   }
   if(password.value.length<6)
   {
-    document.querySelector("#passwordsuggest").style.display = "block"
+    password.setCustomValidity("password too short min 6 charachters")
+    password.reportValidity();
     password.classList.add("addborder")
+  }
+  else
+  {
+    password.classList.remove("addborder")
+    password.classList.add("addborderpos")
   }
 
 
   if (password.value !== c_password.value)
   {
-    document.querySelector("#c_passwordsuggest").style.display = "block"
+    // document.querySelector("#c_passwordsuggest").style.display = "block"
+    c_password.setCustomValidity("password mismatch")
+    c_password.reportValidity(); 
     c_password.classList.add("addborder")
 
   } 
+  else
+  {
+    c_password.classList.remove("addborder")
+    c_password.classList.add("addborderpos")
+  }
+  const emailValue = email.value.trim();
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailRegex.test(emailValue)) {
+    email.setCustomValidity("Invalid email format");
+    email.reportValidity();
+    email.classList.add("addborder");}
+    else
+    {
+      email.classList.remove("addborder")
+      email.classList.add("addborderpos")
+    }
+  e.preventDefault();
 
 
 
